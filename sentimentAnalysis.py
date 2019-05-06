f095089f10529754f569bdb0f5485ef47a66deb7
kmm913 <kmahoney@hawk.iit.edu>
2019-05-06 11:48:12 -0500

    finsihed and moved script

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -3,17 +3,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from demo.models import Datacrawl
 
-def prepareLists(db):
-	tweetTrain = []
-	for line in open(db, 'r'):
-		tweetTrain.append(line.strip())	
-
-	tweetTest = []
-	for line in open(db, 'r'):	
-		tweetTest.append(line.strip())
-
-	return tweetTrain, tweetTest
+db = [Datacrawl.objects.all().order_by('-added')]
 
 def prepareData(tweets):
     REPLACE_NO_SPACE = re.compile("[.;:!\'?,\"()\[\]]")
@@ -22,16 +14,10 @@ def prepareData(tweets):
     tweets = [REPLACE_NO_SPACE.sub("", line.lower()) for line in tweets]
     tweets = [REPLACE_WITH_SPACE.sub(" ", line) for line in tweets]
 
-def dataVectorization(tweetTrain, tweetTest):
-	cv = CountVectorizer(binary=True)
-	cv.fit(tweetTrain)
-	X = cv.transform(tweetTrain)
-	X_test = cv.transform(tweetTest)
+    return tweets
 
-	return X, X_test, cv
-
-def buildClassifier(X):
-    target = [1 if i <100 else 0 for i in range(200)]
+def classifyData(X):
+    target = [1 if i <500 else 0 for i in range(1000)]
     X_train,X_val, y_train, y_val = train_test_split(X, target, train_size = 0.75)
 
     for c in [0.01, 0.05, 0.25, 0.5, 1]:
@@ -41,18 +27,11 @@ def buildClassifier(X):
 
     return target
 
-	#     Accuracy for C=0.01: 0.87472
-	#     Accuracy for C=0.05: 0.88368
-	#     Accuracy for C=0.25: 0.88016
-	#     Accuracy for C=0.5: 0.87808
-	#     Accuracy for C=1: 0.87648
-
 def buildFinalModel(X, target, X_test):
     final_model = LogisticRegression(C=0.05)
     final_model.fit(X, target)
     print("Final Accuracy: %s" % accuracy_score(target, final_model.predict(X_test)))
-
-    # Final Accuracy: 0.88128
+    return final_model
 
 def sanityCheck(cv, final_model):
     feature_to_coef = {
@@ -70,3 +49,33 @@ def sanityCheck(cv, final_model):
         feature_to_coef.items(), 
         key=lambda x: x[1])[:5]:
         print (best_negative)
+
+#######################################################################
+
+#create initial lists
+tweetTrain = []
+for line in open(db, 'r'):
+	tweetTrain.append(line.strip())	
+
+tweetTest = []
+for line in open(db, 'r'):	
+	tweetTest.append(line.strip())
+
+#clean list formatting
+tweetTrain_Clean = prepareData(tweetTrain)
+tweetTest_Clean = prepareData(tweetTest)
+
+#data vectorization
+cv = CountVectorizer(binary=True)
+cv.fit(tweetTrain)
+X = cv.transform(tweetTrain)
+X_test = cv.transform(tweetTest)
+
+#create targets
+target = classifyData(X)
+
+#build final model
+final_model = buildFinalModel(X, target, X_test)
+
+#run sanity check
+sanityCheck(cv, final_model)
