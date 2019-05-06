db85b277c327b0356a60f60c95898f8c286c9855
kmm913 <kmahoney@hawk.iit.edu>
2019-05-06 13:59:31 -0500

    nearly working, but alas, ran out of time

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -1,24 +1,27 @@
+import django
+django.setup()
 import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from demo.models import Datacrawl
+from OSINT.settings import *
 
-db = [Datacrawl.objects.all().order_by('-added')]
+db = [Datacrawl.objects.all()]
+
+REPLACE_NO_SPACE = re.compile("[.;:!\'?,\"()\[\]]")
+REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
 
 def prepareData(tweets):
-    REPLACE_NO_SPACE = re.compile("[.;:!\'?,\"()\[\]]")
-    REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
-
     tweets = [REPLACE_NO_SPACE.sub("", line.lower()) for line in tweets]
     tweets = [REPLACE_WITH_SPACE.sub(" ", line) for line in tweets]
 
     return tweets
 
 def classifyData(X):
-    target = [1 if i <500 else 0 for i in range(1000)]
-    X_train,X_val, y_train, y_val = train_test_split(X, target, train_size = 0.75)
+    target = [1 if i < 64 else 0 for i in range(41)]
+    X_train, X_val, y_train, y_val = train_test_split(X, target, train_size = 0.75)
 
     for c in [0.01, 0.05, 0.25, 0.5, 1]:
         lr = LogisticRegression(C=c)
@@ -54,22 +57,26 @@ def sanityCheck(cv, final_model):
 
 #create initial lists
 tweetTrain = []
-for line in open(db, 'r'):
-	tweetTrain.append(line.strip())	
+for line in db[0]:
+	tweetTrain.append(str(line))
 
 tweetTest = []
-for line in open(db, 'r'):	
-	tweetTest.append(line.strip())
+for line in db[0]:
+	tweetTest.append(str(line))
 
 #clean list formatting
 tweetTrain_Clean = prepareData(tweetTrain)
 tweetTest_Clean = prepareData(tweetTest)
 
+print(tweetTrain_Clean)
+print(tweetTest_Clean)
+
 #data vectorization
 cv = CountVectorizer(binary=True)
-cv.fit(tweetTrain)
-X = cv.transform(tweetTrain)
-X_test = cv.transform(tweetTest)
+cv.fit(tweetTrain_Clean)
+X = cv.transform(tweetTrain_Clean)
+print(X.shape)
+X_test = cv.transform(tweetTest_Clean)
 
 #create targets
 target = classifyData(X)
